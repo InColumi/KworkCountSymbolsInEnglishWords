5a36734c7920490d1c5f14d8124b14ad8ebd0e55
Maxim <max77648@gmail.com>
2021-11-19 11:05:57 +0300

    Закончил

--- KworkCountSymbolsInEnglishWords/main.cpp
+++ KworkCountSymbolsInEnglishWords/main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <string>
-#include <map>
 
 using namespace std;
 
-int ConvertNumberToLengthWord(int number)
+int GetLengthEnglishWordth(int number)
 {
-	int value = 0;
+	int result = 0;
 	switch(number)
 	{
 		case 1: // one
 		case 2: // two
 		case 6: // six
-			value = 3;
+			result = 3;
+			break;
 		case 4: // four
 		case 5: // five
 		case 9: // nine
-			value = 4;
+			result = 4;
 			break;
 		case 3: // three
 		case 7: // seven
@@ -24,7 +24,7 @@ int ConvertNumberToLengthWord(int number)
 		case 40: // forty
 		case 50: // fifty
 		case 60: // sixty
-			value = 5;
+			result = 5;
 			break;
 		case 11: // eleven
 		case 12: // twelve
@@ -32,36 +32,63 @@ int ConvertNumberToLengthWord(int number)
 		case 30: // thirty
 		case 80: // eighty
 		case 90: // ninety
-			value = 6;
+			result = 6;
 			break;
-
 		case 15: // fifteen
 		case 16: // sixteen
 		case 70: // seventy
-			value = 7;
+			result = 7;
 			break;
-
 		case 14: // fourteen
 		case 18: // eighteen
 		case 19: // nineteen
-			value = 8;
+			result = 8;
 			break;
-
 		case 13: // thierteen
 		case 17: // seventeen
-			value = 9;
+			result = 9;
 			break;
-
 		default:
-			throw std::invalid_argument("Unknow number " + number);
+			//throw std::invalid_argument("Unknow number " + number);
+			result = 0;
 			break;
 	}
-	return value;
+	return result;
 }
 
+int Convert(int number)
+{
+	int hunred = number / 100.0; // вычисление сотен
+	int dozens = number - hunred * 100; // вычисление десятков
+	int unit = dozens - (int)(dozens / 10.0) * 10; // вычисление единци
+	int result = 0;
+	if(hunred >= 1 && hunred <= 9)
+	{
+		result += 7 + 3; // hundred(7) + and(3)
+		result += GetLengthEnglishWordth(hunred);
+	}
+
+	if(dozens >= 10 && dozens <= 19)
+	{
+		result += GetLengthEnglishWordth(dozens);
+	}
+	else
+	{
+		result += GetLengthEnglishWordth(dozens - unit);
+		result += GetLengthEnglishWordth(unit);
+	}
+
+	return result;
+}
 
 int main()
 {
+	int sum = 0;
+	for(size_t i = 1; i <= 1000; i++)
+	{
+		sum += Convert(i);
+	}
+	cout << "sum: " << sum << endl;
 	system("pause");
 	return 0;
 }
